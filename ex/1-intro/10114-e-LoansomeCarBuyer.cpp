91de6e46d46f87c8882f5630afdb9e31a9346dde
Solbiati Alessandro <alexsolbiati@hotmail.it>
2017-08-17 18:55:33 +0200

    Update 10114-e-LoansomeCarBuyer.cpp

--- ex/1-intro/10114-e-LoansomeCarBuyer.cpp
+++ ex/1-intro/10114-e-LoansomeCarBuyer.cpp
@@ -1,30 +1,47 @@
-#include <cstdio>  // WA VERDICT
-int main(){
-	float debt,value,d1,d2,rate;
-	int r,loanmonth,t,cur,m1,m2;
-	scanf("%d %f %f %d",&loanmonth,&value,&debt,&t);
-	while(loanmonth>=0){
-		value = value + debt;
-		rate = debt/loanmonth;
-		scanf(" %d %f",&m1,&d1);
-		if(--t>0) scanf(" %d %f",&m2,&d2);
-		else m2=-1;
-		value -= value*d1;
-		cur=0;
-		while(value<debt){
-			cur++;
-			if(cur==m2){
-				m1=m2;
-				d1=d2;
-				if(--t>0) scanf(" %d %f",&m2,&d2);
-				else m2=-1;
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+
+float owe(float N, float c, float L, float n, float i) {return L*(1 - (i / N));}
+float value(float N, float c, float L, float n, float i, std::vector<float> p) {
+	float pp = 1;
+	for (std::vector<float>::iterator p_i = p.begin(); p_i != p.end(); ++p_i) pp = pp*(*p_i);
+	return (L + c)*pp;
+}
+
+int main()
+{
+	while(1)
+	{
+		float N, c, L, n;
+		std::vector<float> p;
+		std::vector<float>::const_iterator first = p.begin();
+
+		scanf("%f %f %f %f", &N, &c, &L, &n);
+		if (N > 0) {
+			//calculate p vector
+			int count, count_prev; float rate, temp;
+			scanf("%d %f", &count_prev, &temp);
+			while (n-- > 1) {
+				scanf("%d %f", &count, &rate);
+				for (int i = count_prev; i < count; i++) p.push_back(1 - temp);
+				temp = rate;
+				count_prev = count;
 			}
-			value -= value*d1;
-			debt -= rate;
+			for (int i = 0; i < 100; i++) p.push_back(1 - temp);
+
+			int flag = 1;
+			for (int i = 1; i < L / N && flag; i++)
+			{
+				std::vector<float> pp(&p[0], &p[i]);
+				if (owe(N, c, L, n, i) < value(N, c, L, n, i, pp)) {
+					if (i == 1) printf("%d month\n", i);
+					else printf("%d months\n", i);
+					flag = 0;
+				}
+
+			}
 		}
-		if(cur==0) cur=1;
-		printf("%d mont%s\n",cur,cur>1 ? "hs" : "h");
-		scanf(" %d %f %f %d",&loanmonth,&value,&debt,&t);
-	}
-	return 0;
+		else break;
+	} 
 }
